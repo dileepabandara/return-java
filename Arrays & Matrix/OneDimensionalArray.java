4d04225d85433bdc223711622ddeaa350583c48f
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-05-03 17:03:36 +0530

    Create OneDimensionalArray.java

---
+++ Arrays & Matrix/OneDimensionalArray.java
@@ -0,0 +1,17 @@
+public class OneDimensionalArray {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
+        arr[3] = 40;
+        arr[4] = 50;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Array Index: " + i + " | Value: " + arr[i]);
+        }
+
+    }
+
+}
